37dd06bd879a17b97b96c768111f49778d9b1501
Yassine <yassine_oumakar@hotmail.com>
2024-01-19 15:00:38 +0100

    création du fichier index.blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Accueil</h1>
+    <p>
+        Vous êtes sur la page d'accueil.
+    </p>
+</body>
+</html>
